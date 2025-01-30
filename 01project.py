cf465870d9a60b1d571d602a2dd063da081c2ab4
sasikumar <bathalasasi25@gmail.com>
2025-01-30 22:40:43 +0530

    01project

---
+++ 01project.py
@@ -0,0 +1,13 @@
+name = input("Enter your name frnd :")
+adj =input("Enter your adjective here :")
+hobby =input("Enter your hobby here :")
+course =input("enter your course here :")
+place =input("Enter your place here :")
+print("\n HERE'S YOUR INTRODUCTION ")
+print(f" Heloo viewers i am {name}.I am a very {adj}Person.")
+print(f"Present i am a{course} student viewers,and i love {hobby}.")
+print(f"I am from {place} and {place } is my native place and i love my native most of the time i spend time in my native.")
+print("Thank you for your patience towards knowing about my self and NiCE TO MEET YOU!")
+
+
+
