ed601000528d86490de6c117f3d86a33c7708a77
sasikumar <bathalasasi25@gmail.com>
2025-02-02 00:36:50 +0530

    basic prob

---
+++ convertingtemp.py
@@ -0,0 +1,6 @@
+#cinverting temperature units
+#temp to celcius
+c = int(input("enter a value:"))
+f = c*(9/5)+35
+k = 273+c
+print(f"foreign heatis:{f},and kelvin is{k}")
---
+++ currencyconverter.py
@@ -0,0 +1,4 @@
+#currency converter basic
+usd = 0.85
+usd_eur = 0.85*100
+print(f"Equivalent amount in EUR:{usd_eur}")
