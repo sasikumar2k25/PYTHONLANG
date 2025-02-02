f5639edbfc043974f816c978f2b4fd96eca50446
sasikumar <bathalasasi25@gmail.com>
2025-02-02 17:10:48 +0530

    if condition

---
+++ if.py
@@ -0,0 +1,16 @@
+#if condition 
+ 
+#q = input()
+
+#if q=="should code":
+    print("The statement is true so have to do code this total day !!Must and should")
+# here the space between before the print is called induntation
+print("The statement is false so go and fuck the day!")
+#if the statement is false it will print the above print section which is with without induntation
+  
+
+  #quiz problem
+  s = input()
+
+if s=="yes":
+   print(" you entered yes ")
