2952405004d153ce9a7842552c0801ad0e8b4be4
sasikumar <bathalasasi25@gmail.com>
2025-02-02 19:22:35 +0530

    ifelifelse condition

---
+++ ifelifelse.py
@@ -0,0 +1,18 @@
+#module example
+
+weather = "time"
+quanta = "life"
+
+if weather=="liife":
+    print("life is short soo enjoy !!")
+elif quanta=="raju":
+    print("hello guys")
+elif weather=="time" and quanta=="life":      # here and operators is taken from logical operators
+    print(" If u utilize time and life u will be defenetly rock ur future!! ")
+else:
+    print("none of the above")
+
+
+
+
+
---
+++ ifelifelse2.py
@@ -0,0 +1,14 @@
+    # if elif else condition
+
+s = input()
+
+if s=="done":
+    print("ok we should play a game!!")
+
+elif s=="not done":
+    #print("we can play nxt time guys!!")
+
+elif s=="never done":
+    print("get lost !!") # here we can give infinite number of elif conditions and should give only one if and else 
+else :
+    print("byee byee mawaaa ")
