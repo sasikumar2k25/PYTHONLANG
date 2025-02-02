4348612aa6ee1956693ec7a56202f153b36e89d1
sasikumar <bathalasasi25@gmail.com>
2025-02-02 18:32:18 +0530

    ifelse condition

--- ifelse.py
+++ ifelse.py
@@ -6,5 +6,14 @@ if q=="code":
 else :
     print("dont do code play the game what yoyu wanted to play!!")
 
-    
-print("dont do both of the work go to bed and sleep")   
+
+print("dont do both of the work go to bed and sleep")  
+
+#problem from quiz
+
+x = input("enter 'yes' or 'no':")
+ 
+if x=="yes":
+    print("entered yes ")
+else :
+    print("entered No bhaiii")
