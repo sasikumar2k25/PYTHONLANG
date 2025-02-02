05e119a8124fa1eebf30c0d8b3316252f376a413
sasikumar <bathalasasi25@gmail.com>
2025-02-02 18:24:59 +0530

    if else condition

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+#using ifelse condition
+q = input()
+
+if q=="code":
+    print("you should code man!! ")
+else :
+    print("dont do code play the game what yoyu wanted to play!!")
+
+    
+print("dont do both of the work go to bed and sleep")   
