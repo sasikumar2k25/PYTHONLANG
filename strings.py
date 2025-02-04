5aa37034485da819140750c313af87c1249a05c8
sasikumar <bathalasasi25@gmail.com>
2025-02-04 23:10:41 +0530

    strings module

---
+++ strings.py
@@ -0,0 +1,30 @@
+#STRINGS
+var = "sky"
+print(var)
+var1 = 'sky'
+print(var1)
+var2 = """Hey !hii this is sasikumar 
+           from naryanapuram located at 
+           nellore district."""
+print(var2)
+# string indexing
+var3 = "sasikumaryadav"
+print(var3[4])
+
+#string slicing
+var4 ="sasikumaryadav"
+print(var4[0:6])#here 0:6 means from starting value to end before value means it prints[0:5]
+ 
+
+var5 = "sasikumaryadav"
+print(var5[0::2])#[ ::2] is also equal to [0::2]
+ 
+
+var6 = "bathala sasikumar"
+print(var6[1::2])
+print(var6[::-1])#it will reverse the given string
+
+#string escaping
+
+var7 ="sasi\"s world "#by using back slash we can do ,enter into new line by '\n'.
+print(var7)
