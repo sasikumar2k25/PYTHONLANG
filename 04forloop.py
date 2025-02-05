6130af956f6f3be9cbab3c22b8afee791c8baae0
sasikumar <bathalasasi25@gmail.com>
2025-02-05 23:13:24 +0530

    loops

---
+++ 04forloop.py
@@ -0,0 +1,20 @@
+# for loop condition
+
+candies = 10
+
+for i in range(0,candies):
+    print("helo mawa ala unaii candies kavala!!")
+
+
+#another for loop condition
+
+msg = "Hello world!!"
+
+for i in msg:
+    print(i)
+
+
+#length = len(msg)
+
+#or i in range(length):
+    print(i)
---
+++ 04whileloop.py
@@ -0,0 +1,32 @@
+# while loop is used to print more than once 
+#its syntax will be common as decision making
+
+chocolates = 10
+
+while chocolates >0:
+    print ("i will give those to my friends")
+    chocolates -= 1
+
+print(chocolates)
+
+
+#quiz problem
+
+count = 0
+while count<5:
+    print(count)
+    count += 1
+#quiz problem
+count = 3
+while count>0:
+    print("hello")
+    count -=1
+
+#quiz problem
+
+count = 0
+while count<5:
+    print(count)
+    count +=1
+else:
+    print("loop is complete")
