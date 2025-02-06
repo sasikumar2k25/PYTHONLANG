33cec4d32b19a8bf3dbb8d304a69564e6180c66c
sasikumar <bathalasasi25@gmail.com>
2025-02-06 23:34:15 +0530

    loops

---
+++ 0041toN.py
@@ -0,0 +1,13 @@
+#print numbers from 1 to N
+
+n = int(input("enter a number:"))
+
+i = 1
+
+#while i <= n: # using while loop
+   # print(i)
+   # i +=1 #i = i+1
+
+for i in range(1,13): #using for loop
+    print(i)
+     
